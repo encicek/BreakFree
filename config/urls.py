f41c0cd0fdacde07a159d77af2d49c1a016f656a
encicek <eda.n.cicek@gmail.com>
2026-05-04 17:30:11 +0300

    Render için

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.contrib.auth import views as auth_views # Eksik olan ve hatayı çözen satır bu!
+from django.contrib.auth import views as auth_views
+from django.views.generic import RedirectView # Ana sayfa yönlendirmesi için ekledik
 
 # Admin Paneli Özelleştirmeleri
 admin.site.site_header = "BreakFree Yönetim Paneli"
@@ -24,8 +25,11 @@ admin.site.site_title = "BreakFree Admin"
 admin.site.index_title = "Sistem Yönetimine Hoş Geldiniz"
 
 urlpatterns = [
+    # Boş yolu (ana sayfayı) doğrudan login'e veya tracking'e yönlendiriyoruz
+    # Bu satır Render'daki o sarı 404 sayfasını engelleyecek:
+    path('', RedirectView.as_view(url='login/', permanent=True)), 
+
     path('admin/', admin.site.urls),
-    # Login sayfası için auth_views artık tanımlı olduğu için hata vermeyecek
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html', next_page='/tracking/'), name='login'),
     path('tracking/', include('tracking.urls')),
     path('community/', include(('community.urls', 'community'), namespace='community')),
