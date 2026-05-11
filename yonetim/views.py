89bad1e83f1b58a1375d37df82123d6fccaebb44
encicek <eda.n.cicek@gmail.com>
2026-05-11 13:53:30 +0300

    dfs

--- yonetim/views.py
+++ yonetim/views.py
@@ -1,8 +1,7 @@
-# yonetim/views.py
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
 
-@staff_member_required(login_url='/accounts/login/') # Yetkisi olmayan login'e atılır
+# 🚨 LOGIN_URL KISMINI GİZLİ ADMİN OLARAK DEĞİŞTİRDİK 🚨
+@staff_member_required(login_url='/gizli-admin/login/') 
 def dashboard(request):
-    # İleride buraya veritabanından kullanıcı sayılarını vs. çekeceğiz
     return render(request, 'yonetim/dashboard.html')
