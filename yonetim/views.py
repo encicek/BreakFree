b4430d79211e00d1ff758251aa7ae44110b82675
encicek <eda.n.cicek@gmail.com>
2026-05-11 14:05:51 +0300

    çok korkuyom

--- yonetim/views.py
+++ yonetim/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.contrib.admin.views.decorators import staff_member_required
+from django.contrib.auth.models import User
+from tracking.models import DependencyGoal
+from community.models import Post
 
-# 🚨 LOGIN_URL KISMINI GİZLİ ADMİN OLARAK DEĞİŞTİRDİK 🚨
 @staff_member_required(login_url='/gizli-admin/login/') 
 def dashboard(request):
-    return render(request, 'yonetim/dashboard.html')
+    # Veritabanından İstatistikleri Çekiyoruz
+    total_users = User.objects.count()
+    total_goals = DependencyGoal.objects.count()
+    total_posts = Post.objects.count()
+
+    context = {
+        'total_users': total_users,
+        'total_goals': total_goals,
+        'total_posts': total_posts,
+    }
+    
+    return render(request, 'yonetim/dashboard.html', context)
