7adbdb30ef9bd7b0157f38aefa224f3e882136f5
encicek <eda.n.cicek@gmail.com>
2026-05-11 14:28:41 +0300

    şlşşşş

--- yonetim/views.py
+++ yonetim/views.py
@@ -18,23 +18,21 @@ def dashboard(request):
     }
     return render(request, 'yonetim/dashboard.html', context)
 
-# 🚨 YENİ FONKSİYON: KULLANICI LİSTESİ VE ARAMA 🚨
+# yonetim/views.py
+
 @staff_member_required(login_url='/gizli-admin/login/') 
 def user_list(request):
-    # Arama kutusuna bir şey yazılmış mı diye bakıyoruz
     query = request.GET.get('q', '') 
     
     if query:
-        # Eğer bir şey yazıldıysa, kullanıcı adı veya e-posta içinde bunu ara
         users = User.objects.filter(
             Q(username__icontains=query) | Q(email__icontains=query)
-        ).order_by('-date_joined') # En son kayıt olan en üstte
+        ).prefetch_related('earned_badges__badge', 'goals').order_by('-date_joined')
     else:
-        # Arama yapılmadıysa herkesi getir
-        users = User.objects.all().order_by('-date_joined')
+        users = User.objects.all().prefetch_related('earned_badges__badge', 'goals').order_by('-date_joined')
 
     context = {
         'users': users,
-        'query': query, # Arama kutusunda yazılan silinmesin diye geri gönderiyoruz
+        'query': query,
     }
     return render(request, 'yonetim/user_list.html', context)
