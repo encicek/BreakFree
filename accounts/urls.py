25dbac4d186ed6edbc4f3a6e6d53428c328d5277
Fatma Zülal Baltacı <fzulalbaltaci@gmail.com>
2026-05-15 11:05:40 +0300

    şifre değiştirme ekledim

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,7 +7,10 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name='login'),
     
     # accounts/urls.py
-path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
     
     path('register/', views.register, name='register'),
+
+    path('password-change/', auth_views.PasswordChangeView.as_view(template_name='accounts/password_change.html'), name='password_change'),
+    path('password-change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='accounts/password_change_done.html'), name='password_change_done'),
 ]
